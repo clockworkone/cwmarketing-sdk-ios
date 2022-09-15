0fc1f305fdbf4adad6a60f64c12959b62482bee1
Clockwork, LLC <hello@clockwork.one>
2022-09-15 16:20:27 +0700

    initial commit

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CWMarketing",
-    platforms: [.iOS(.v11), .macOS(.v10_12), .tvOS(.v12), .watchOS(.v7)],
+    platforms: [.iOS(.v11), .macOS(.v10_13), .tvOS(.v12), .watchOS(.v7)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,17 +14,17 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/Alamofire/Alamofire", from: "5.6.0"),
-         .package(url: "https://github.com/Alamofire/AlamofireImage", from: "4.2.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire", .upToNextMajor(from: "5.6.0")),
+        .package(url: "https://github.com/Alamofire/AlamofireImage", .upToNextMajor(from: "4.2.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CWMarketing",
-            dependencies: ["Alamofire"]),
+            dependencies: ["Alamofire", "AlamofireImage"]),
         .testTarget(
             name: "CWMarketingTests",
-            dependencies: ["CWMarketing", "Alamofire"]),
+            dependencies: ["CWMarketing", "Alamofire", "AlamofireImage"]),
     ]
 )
