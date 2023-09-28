ce41054c122569187402774183bf2241bb20841d
Ilia Aparin <hello@clockwork.one>
2023-09-28 16:20:46 +0700

    bump 0.0.57

--- Sources/CWMarketing/CWMarketing.swift
+++ Sources/CWMarketing/CWMarketing.swift
@@ -13,7 +13,7 @@ import CryptoKit
 import os.log
 import CoreData
 
-let version = "0.0.56"
+let version = "0.0.57"
 let uri = "https://customer.api.cw.marketing/api"
 let paymentUri = "https://payments.cw.marketing/v1/create"
 
